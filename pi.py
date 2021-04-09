80c6c405f7848fe2f1415ed29c434d7250c20c70
rtub5 <77343357+rtub5@users.noreply.github.com>
2021-04-09 19:52:45 +0200

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,1 +1,13 @@
+import random 
 
+def calcul_pi(n):
+  punts_cercle = 0 
+  punts_total = 0 
+  for _ in range(n):
+    x = random.uniform(0, 1)
+    y = random.uniform(0, 1)
+    distance = x**2 + y**2
+    if distance <= 1:
+      punts_cercle += 1
+    punts_total += 1 
+  print(4 * punts_cercle/punts_total)
